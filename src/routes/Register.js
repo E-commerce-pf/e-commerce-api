9cddabb833e4ac41a513701145606a36d523fbc2
BuildTools <unconfigured@null.spigotmc.org>
2022-03-15 16:09:45 -0300

    registro y rutas

---
+++ src/routes/Register.js
@@ -0,0 +1,29 @@
+const router = require("express").Router();
+const sequelize = require("../db");
+const User = sequelize.models.User;
+const { encrypt } = require("../controllers/encrypt");
+
+router.post("/", async (req, res) => {
+    //Encriptación de la contraseña
+    let {email,username,password} = req.body;
+    if( !email || !username ||!password ) return res.status(400).json({ error: "Some fields where empty" });
+    req.body.password = encrypt(req.body.password);
+  
+    //Comprobación que no exista un email igual en la base de datos
+    const result = await User.findOne({
+      where: {
+        email
+      },
+    });
+    if (!result) {
+      try {
+        await User.create(req.body);
+        return res.status(200).json({ success: "User created successfuly" });
+      } catch (error) {
+        console.log("Error: ", error);
+        return res.status(400).json(error);
+      }
+    }
+    res.status(400).json({ error: "User already exists" });
+  });
+  module.exports = router;
