91c63dec5dc155a2175f703073b63f1f67fa21e5
Marcelo Gottardini <89224636+Galaximar@users.noreply.github.com>
2022-03-15 16:24:34 -0300

    Delete Routes.js

--- src/routes/Routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-//Enlaze de las rutas
-const registerRoute = require('./Register');
-
-router.use('/register', registerRoute);
-
-module.exports = router;
