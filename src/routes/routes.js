d1a800b2ff0b962eb2629ce3687f0e21c844a964
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-03-17 17:15:01 -0500

    routes.js adaptado para el deploy

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,35 +1,13 @@
-const router = require('express').Router();
-const cookieParser = require("cookie-parser");
-const bodyParser = require("body-parser");
-const morgan = require("morgan");
-
-//DEFINIMOS LOS MIDDLEWARE
-router.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-router.use(bodyParser.json({ limit: "50mb" }));
-router.use(cookieParser());
-router.use(morgan("dev"));
-router.use((req, res, next) => {
-      res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-      res.header("Access-Control-Allow-Credentials", "true");
-      res.header(
-            "Access-Control-Allow-Headers",
-            "Origin, X-Requested-With, Content-Type, Accept"
-      );
-      res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-      next();
-});
+const router = require("express").Router();
 
 //IMPORTAMOS TODAS LAS RUTAS
-const user = require('./User');
-const registerRoute = require('./Register');
-const reviewRoute= require('./Review')
+const user = require("./User");
+const registerRoute = require("./Register");
+const reviewRoute = require("./Review");
 
 //DEFINIMOS LOS CASOS DE USO DE LAS RUTAS
-router.use('/user', user);
-router.use('/register', registerRoute);
-router.use('/review', reviewRoute);
-
-
-
+router.use("/user", user);
+router.use("/register", registerRoute);
+router.use("/review", reviewRoute);
 
 module.exports = router;
