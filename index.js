94712dd335e86554eef8a2eb686de447e26d40db
BrunoAmerio <88524640+BrunoAmerio@users.noreply.github.com>
2022-03-21 18:40:09 -0300

    modificacion de rutas

--- index.js
+++ index.js
@@ -10,30 +10,30 @@ sequelize.sync({ force: false }).then(() => {
   server.listen(PORT, async () => {
     console.log(`Server listening on port ${PORT}`);
 
-    // await Category.bulkCreate(categories);
-    // await Product.bulkCreate(products);
-    // for (let i = 0; i < products.length; i++) {
-    //   const findCategory = await Category.findAll({
-    //     where: {
-    //       name: products[i].categories,
-    //     },
-    //   });
+    await Category.bulkCreate(categories);
+    await Product.bulkCreate(products);
+    for (let i = 0; i < products.length; i++) {
+      const findCategory = await Category.findAll({
+        where: {
+          name: products[i].categories,
+        },
+      });
 
-    //   const [newProduct] = await Product.findOrCreate({
-    //     where: {
-    //       title: products[i].title,
-    //       price: products[i].price,
-    //       image: products[i].image,
-    //       description: products[i].description,
-    //       stock: products[i].stock,
-    //       sales: products[i].sales,
-    //       discount: products[i].discount,
-    //     },
-    //   });
+      const [newProduct] = await Product.findOrCreate({
+        where: {
+          title: products[i].title,
+          price: products[i].price,
+          image: products[i].image,
+          description: products[i].description,
+          stock: products[i].stock,
+          sales: products[i].sales,
+          discount: products[i].discount,
+        },
+      });
 
-    //   await newProduct.setCategories(findCategory);
-    // }
+      await newProduct.setCategories(findCategory);
+    }
 
-    // console.log("Products and categories pre charged :)");
+    console.log("Products and categories pre charged :)");
   });
 });
