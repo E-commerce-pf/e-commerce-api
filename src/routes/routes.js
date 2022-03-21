50003dfe39360d96d386013425df1153dfd1346d
Dairo01001 <garcianaranjodairo@gmail.com>
2022-03-21 16:16:50 -0500

    Reviews Con mayorQue

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -10,15 +10,18 @@ const cartRoute = require("./Cart");
 const categoryRoute = require("./Category");
 const adminRoute = require('./Admin')
 const routerUsers = require("./Users");
+const routerReviews = require("./Reviews");
 
 //DEFINIMOS LOS CASOS DE USO DE LAS RUTAS
 router.use("/user", user);
 router.use("/register", registerRoute);
+router.use("/reviews", routerReviews);
 router.use("/review", reviewRoute);
 router.use("/product", productRoute);
 router.use("/cart", cartRoute);
 router.use("/category", categoryRoute);
 router.use('/admin', adminRoute);
 router.use("/users", routerUsers);
+
 
 module.exports = router;
