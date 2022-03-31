de47de6fbdb18b071800bdf67a03ba19382190eb
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-03-30 19:40:29 -0500

    Index actualizado

--- index.js
+++ index.js
@@ -8,54 +8,54 @@ const sequelize = require("./src/db");
 const { encrypt } = require("./src/controllers/encrypt");
 const { Product, Category, Review, User, Cart } = sequelize.models;
 
-sequelize.sync({ force: false }).then(() => {
+sequelize.sync({ force: true }).then(() => {
   server.listen(PORT, async () => {
     console.log(`Server listening on port ${PORT}`);
 
-    // await Category.bulkCreate(categories);
+    await Category.bulkCreate(categories);
 
-    // for (let i = 0; i < products.length; i++) {
-    //   products[i].image = products[i].image.join("*_*");
-    //   const findCategory = await Category.findAll({
-    //     where: {
-    //       name: products[i].categories,
-    //     },
-    //   });
+    for (let i = 0; i < products.length; i++) {
+      products[i].image = products[i].image.join("*_*");
+      const findCategory = await Category.findAll({
+        where: {
+          name: products[i].categories,
+        },
+      });
 
-    //   const [newProduct] = await Product.findOrCreate({
-    //     where: {
-    //       title: products[i].title,
-    //       price: products[i].price,
-    //       image: products[i].image,
-    //       description: products[i].description,
-    //       stock: products[i].stock,
-    //       sales: products[i].sales || 0,
-    //       discount: products[i].discount || 0,
-    //     },
-    //   });
+      const [newProduct] = await Product.findOrCreate({
+        where: {
+          title: products[i].title,
+          price: products[i].price,
+          image: products[i].image,
+          description: products[i].description,
+          stock: products[i].stock,
+          sales: products[i].sales || 0,
+          discount: products[i].discount || 0,
+        },
+      });
 
-    //   await newProduct.setCategories(findCategory);
-    // }
+      await newProduct.setCategories(findCategory);
+    }
 
-    // for (let i = 0; i < users.length; i++) {
-    //   users[i].password = encrypt(users[i].password);
+    for (let i = 0; i < users.length; i++) {
+      users[i].password = encrypt(users[i].password);
 
-    //   const userCartId = await Cart.create().then(
-    //     ({ dataValues }) => dataValues.id
-    //   );
+      const userCartId = await Cart.create().then(
+        ({ dataValues }) => dataValues.id
+      );
 
-    //   await User.findOrCreate({
-    //     where: {
-    //       cartId: userCartId,
-    //       name: users[i].name,
-    //       lastName: users[i].lastName,
-    //       email: users[i].email,
-    //       password: users[i].password,
-    //       country: users[i].country,
-    //       isAdmin: users[i].isAdmin || false,
-    //     },
-    //   });
-    // }
-    // console.log("Products, users, admins and categories pre charged :)");
+      await User.findOrCreate({
+        where: {
+          cartId: userCartId,
+          name: users[i].name,
+          lastName: users[i].lastName,
+          email: users[i].email,
+          password: users[i].password,
+          country: users[i].country,
+          isAdmin: users[i].isAdmin || false,
+        },
+      });
+    }
+    console.log("Products, users, admins and categories pre charged :)");
   });
 });
