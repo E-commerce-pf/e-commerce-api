acb6fb3fdec08fa0ed977f4e0967ca616acbe95b
Brayan Baquero <78622205+babaquero07@users.noreply.github.com>
2022-04-05 12:21:28 -0500

    Cambio en respuesta al enviar un correo

--- src/controllers/SendEmail.js
+++ src/controllers/SendEmail.js
@@ -32,7 +32,7 @@ const sendEmail = (req, res) => {
       if (err) {
         return res.status(404).json({ error: err.message });
       }
-      return res.json({success: "Email Enviado!"});
+      return res.json({ success: "Done! Please check your email" });
     }
   );
 };
